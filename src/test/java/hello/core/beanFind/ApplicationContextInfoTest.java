2b7b092414beac6e176f3b65796591920c55cb30
sunghyeok <glxm06052@gmail.com>
2022-09-19 10:05:30 +0900

    1

--- src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -19,14 +19,18 @@ public class ApplicationContextInfoTest {
             System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
         }
         
-    }    @Test
+    }
+
+    @Test
     @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationBean(){
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+            //Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈빈
+           if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
             }

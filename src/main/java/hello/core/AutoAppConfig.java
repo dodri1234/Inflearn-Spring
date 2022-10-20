2bed089b969756a1fa65f3bcdaf3ef8531bd4853
sunghyeok <glxm06052@gmail.com>
2022-10-20 15:42:58 +0900

    1

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
